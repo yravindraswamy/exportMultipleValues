819d8289efdf862e24b9147436f3e2153f456871
root <yravindraswamy@gamil.com>
2021-04-13 10:06:26 +0000

    exporting multiple values

---
+++ exportMultipleValues.mjs
@@ -0,0 +1,9 @@
+const myArray = Array("camel", 265, true, "5.6");
+const bulb = {
+  watts: 10,
+  type: "LED",
+};
+function multiplyByFour(number) {
+  return number * 4;
+}
+export { myArray, bulb, multiplyByFour };
